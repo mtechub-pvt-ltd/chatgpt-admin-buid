b4ede860fb9d0ba7bb533cbe7cb7e967259c4a78
mtechub-pvt-ltd <mtechub.info@gmail.com>
2023-03-04 14:31:26 +0500

    final commit by usama

---
+++ precache-manifest.0d6b6dfaff89a832f4ea4328c522ab1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b38e8c231bdd1187e8b",
+    "url": "/static/css/main.944c6567.chunk.css"
+  },
+  {
+    "revision": "3b38e8c231bdd1187e8b",
+    "url": "/static/js/main.3b38e8c2.chunk.js"
+  },
+  {
+    "revision": "133bd598701594277177",
+    "url": "/static/css/1.66607c9a.chunk.css"
+  },
+  {
+    "revision": "133bd598701594277177",
+    "url": "/static/js/1.133bd598.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "076644be402bc7ecf25f6934737a9242",
+    "url": "/static/media/logo.076644be.png"
+  },
+  {
+    "revision": "c8bfeb94a04dfe9d2c1b3f17d107fe44",
+    "url": "/static/media/illution.c8bfeb94.png"
+  },
+  {
+    "revision": "42eaac3afa7ef7f20d428b506e3d90bb",
+    "url": "/index.html"
+  }
+];
